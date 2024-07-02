0495fa5e0164271e48dc0c2e903d0cb15f1f836b
unknown <priya sharma>
2024-07-02 19:36:10 +0530

    added congratulation audio file and updated the game logic

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,121 +1,108 @@
-let boxes=document.querySelectorAll(".box");
-let resetBtn=document.querySelector("#New-Game");
-let msgContainer=document.querySelector(".msg-container");
-let msg =document.querySelector("#msg")
 
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#New-Game");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
+// Audio element
+const winSound = new Audio('images/congratulation_audio.mp3'); // Replace with the path to your winner sound file
 
-let turnO= true;   //player//
-let count=0;
+let turnO = true;   // Player turn
+let count = 0;
+let gameEnded = false; // Flag to track if the game has ended
 
-//defining the all winning possibilities//
+// Defining all winning possibilities
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 
-const winPatterns =
-[
-[0,1,2],
-[0,3,6],
-[0,4,8],
-[1,4,7],
-[2,5,8],
-[2,4,6],
-[3,4,5],
-[6,7,8]];
-
-
-//to reset the gamee
-const NewGame=()=>{
-    turnO=true;
+// Reset the game
+const NewGame = () => {
+    turnO = true;
     count = 0;
-    enableBoxes()
-    msg.innerText="turn of player O"
-    boxes.forEach((box,index)=>{
-    box.classList.remove("winner")
+    gameEnded = false; // Reset the game ended flag
+    enableBoxes();
+    msg.innerText = "Turn of player O";
+    boxes.forEach((box, index) => {
+        box.classList.remove("winner");
+        box.innerText = "";
     });
+};
 
-}
-
-
-///function in case of draw condition//
+// Function in case of draw condition
 const gameDraw = () => {
     msg.innerText = "Game was a Draw.";
-   
     disableBoxes();
-  };
+};
 
-
-// function to show the turn// 
-const showTurn=()=>{
-    if(turnO==true){
-        msg.innerText="turn of player X"
-        
+// Function to show the turn
+const showTurn = () => {
+    if (turnO) {
+        msg.innerText = "Turn of player X";
+    } else {
+        msg.innerText = "Turn of player O";
     }
-    else{
-        msg.innerText="turn of player O"
-    }
+};
 
-    
- }
-//adding functionality to the boxes on click and at every step we are checking the winner//
-boxes.forEach((box)=>{
-    box.addEventListener('click',() => {
-       
-       if(turnO==true){
-        showTurn();
-        box.innerText="O";
-        turnO=false;
-        
-       }else{
-        showTurn();
-        box.innerText="X";
-        turnO=true;
-       }
-       box.disabled=true;
+// Adding functionality to the boxes on click and checking the winner at every step
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        if (turnO) {
+            showTurn();
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            showTurn();
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
 
+        // To check winner
+        count++;
 
-//to check winner//
-    count++;
+        let isWinner = checkWinner();
 
-       let isWinner = checkWinner();
-   
-       if (count === 9 && !isWinner) {
-         gameDraw();
-       }
+        if (count === 9 && !isWinner) {
+            gameDraw();
+        }
     });
 });
 
-
-
- 
-// after the winner disabled all thr boxes//
- const disableBoxes=()=>{
-    for(let box of boxes){
-        box.disabled=true;
+// Disable all boxes after the winner
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
     }
- }
+};
 
-
- //to reset the game enable all the boxes//
- const enableBoxes=()=>{
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText=""
+// Enable all the boxes to reset the game
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
     }
- }
+};
 
+// Function to show the winner
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    disableBoxes();
+    gameEnded = true; // Set the game ended flag
 
- //function to show thw winner//
- const showWinner=(winner)=>{
-     msg.innerText=`Congratulations,Winner is ${winner}`
-    
-     disableBoxes()
-     
-    
+    // Play win sound if game has not ended before
+    winSound.play();
+};
 
- }
-
- //////fireeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-
- const fireConfetti = () => {
+// Function to initialize confetti
+const fireConfetti = () => {
     var confettiSettings = {
         spread: 2000,
         startVelocity: 55,
@@ -126,38 +113,26 @@ boxes.forEach((box)=>{
     confetti(confettiSettings);
 };
 
-//checkwinner at every click//
-const checkWinner=()=>{
-    for(let pattern of winPatterns){
-        
-        let pos1val=boxes[pattern[0]].innerText;
-        let pos2val=boxes[pattern[1]].innerText;
-        let pos3val=boxes[pattern[2]].innerText;
-        if(pos1val!=""&&pos2val!=""&&pos3val!=""){
-            if(pos1val===pos2val&&pos2val===pos3val){
-                boxes[pattern[0]].classList.add("winner") 
-                boxes[pattern[1]].classList.add("winner") 
-                boxes[pattern[2]].classList.add("winner") 
-                
-                showWinner(pos1val);
-                ////this is the giff ////////////////////////
-                // confetti();
-                fireConfetti();
-            
-            
+// Check winner at every click
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+        if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                boxes[pattern[0]].classList.add("winner");
+                boxes[pattern[1]].classList.add("winner");
+                boxes[pattern[2]].classList.add("winner");
 
-                
+                showWinner(pos1val);
+                fireConfetti();
 
                 return true;
             }
-            
-            
         }
-        
-        
     }
-  
-}
+};
 
-//reset the game using New Game button//
-resetBtn.addEventListener("click",NewGame)
+// Reset the game using New Game button
+resetBtn.addEventListener("click", NewGame);
